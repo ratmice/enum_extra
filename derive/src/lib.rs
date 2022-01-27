5ef48a790b68d3cdd2b21460684b9a7cc27338fe
matt rice <ratmice@gmail.com>
2022-01-27 11:05:05 -0800

    initial support for non-literal NonZeroRepr...

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -12,37 +12,38 @@ pub fn non_zero_repr(input: TokenStream) -> TokenStream {
 
 fn impl_non_zero_repr(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
-    match &ast.data {
-        syn::Data::Enum(e) => {
-            for v in &e.variants {
-                match v {
-                    syn::Variant {
-                        discriminant:
-                            Some((
-                                _,
-                                syn::Expr::Lit(syn::ExprLit {
-                                    lit: syn::Lit::Int(value),
-                                    ..
-                                }),
-                            )),
-                        ..
-                    } => {
-                        let discriminant = value.base10_parse::<u64>().unwrap();
-                        assert!(discriminant != 0)
-                    }
-                    _ => {
-                        panic!("Discriminant not a literal int.");
-                    }
-                }
-            }
+    let variants = if let syn::Data::Enum(e) = &ast.data {
+        e.variants.iter()
+    } else {
+        panic!("NonZeroRepr only supports enums");
+    };
+
+    let discriminants = variants.filter_map(|v| match v {
+        syn::Variant {
+            discriminant: Some((_, d)),
+            ..
+        } => Some(d),
+        _ => None,
+    });
+
+    let discriminants = discriminants.filter_map(|d| match d {
+        syn::Expr::Lit(syn::ExprLit {
+            lit: syn::Lit::Int(value),
+            ..
+        }) => {
+            let discriminant = value.base10_parse::<u64>().unwrap();
+            assert!(discriminant != 0);
+            None
         }
-        _ => {
-            panic!("Not an enum");
-        }
-    }
+
+        _ => Some(d),
+    });
+
     let impl_body = quote!(
+        #( const _: () = if ::core::num::NonZeroU8::new(#discriminants).is_some() { () } else { panic!("Expected non-zero discriminant") }; )*
         impl NonZeroRepr for #name {
         }
+
     );
     impl_body.into()
 }
